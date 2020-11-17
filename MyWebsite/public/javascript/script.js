27ce892a28f9781c81f1ae95878c8c17c7a828a7
Guzzard69 <khan7192020@gmail.com>
2020-11-17 01:36:11 -0500

    added images

--- MyWebsite/public/javascript/script.js
+++ MyWebsite/public/javascript/script.js
@@ -1,34 +1,32 @@
-window.onload = () => {
-    console.log('javascript loaded');
+console.log('javascript loaded');
 
-    //text animation
-    let counter = 0;
-    let letterArray = [];
-    let isFinished = false;
-    let animationBox = document.querySelector('#animation-text');
-    let myString = animationBox.innerHTML;
-    const Write = () => {
-        if (counter < myString.length && !isFinished) {
-            letterArray.push(myString[counter]);
-            animationBox.innerHTML = letterArray.join("");
-            counter++;
-        }
-        else if (counter == myString.length) {
-            setTimeout(() => {
-                isFinished = true;
-                letterArray.pop(myString[counter]);
-                animationBox.innerHTML = letterArray.join("");
-                counter--;
-            }, 1000);
-        }
-        else if (counter > 1 && isFinished) {
+//text animation
+let counter = 0;
+let letterArray = [];
+let isFinished = false;
+let animationBox = document.querySelector('#animation-text');
+let myString = animationBox.innerHTML;
+const Write = () => {
+    if (counter < myString.length && !isFinished) {
+        letterArray.push(myString[counter]);
+        animationBox.innerHTML = letterArray.join("");
+        counter++;
+    }
+    else if (counter == myString.length) {
+        setTimeout(() => {
+            isFinished = true;
             letterArray.pop(myString[counter]);
             animationBox.innerHTML = letterArray.join("");
             counter--;
-        }
-        else {
-            isFinished = false;
-        }
+        }, 1000);
     }
-    setInterval(Write, 50);
+    else if (counter > 1 && isFinished) {
+        letterArray.pop(myString[counter]);
+        animationBox.innerHTML = letterArray.join("");
+        counter--;
+    }
+    else {
+        isFinished = false;
+    }
 }
+setInterval(Write, 50);
