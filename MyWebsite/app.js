1bcfcc33444a5896cbb35dd579d869665f18514b
Guzzard69 <khan7192020@gmail.com>
2020-11-18 00:27:52 -0500

    fixed port

--- MyWebsite/app.js
+++ MyWebsite/app.js
@@ -10,6 +10,5 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', pages);
 
-//const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, console.log(`listening on port ${port}`));
