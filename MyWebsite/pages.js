1a4328aa128a06346077863cd14853d0db993f3e
Guzzard69 <khan7192020@gmail.com>
2020-11-17 22:58:34 -0500

    added pokemon

--- MyWebsite/pages.js
+++ MyWebsite/pages.js
@@ -5,8 +5,4 @@ router.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
 });
 
-router.get('/test', (req, res) => {
-    res.sendFile(`${__dirname}/views/index-test.html`);
-});
-
 module.exports = router;
--- MyWebsite/public/javascript/script.js
+++ MyWebsite/public/javascript/script.js
@@ -30,3 +30,64 @@ const Write = () => {
     }
 }
 setInterval(Write, 50);
+
+
+//Pokemon Logic
+let pokemonName;
+let totalScore = 0;
+let totalPokemon = 0;
+let hasScored = false;
+
+const getPokemon = () => {
+    let request = new XMLHttpRequest;
+    request.open('GET', `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 893) + 1}`)
+    request.send();
+
+    //shows loading gid and clears submission box
+    document.querySelector('#loading').src = '/images/loading.gif';
+    document.querySelector('#pokemonBox').value = '';
+
+    request.onload = () => {
+        let data = JSON.parse(request.responseText);
+
+        document.querySelector('#pokemonImg').src = data.sprites.front_default;
+
+        let name = data.species.name;
+        let upperName = name[0].toUpperCase() + name.slice(1)
+        pokemonName = upperName.replace('-', ' ');
+        document.querySelector('#loading').src = '';
+
+        hasScored = false;
+    }
+}
+getPokemon();
+
+//checks if the submission is a new correct answer and upticks score
+const UpScore = () => {
+    if (!hasScored) {
+        totalScore++;
+        document.querySelector('#score').innerHTML = `Total Score: ${totalScore}`;
+    }
+}
+
+//gets a new pokemon for new pokemon button
+document.querySelector("#pokemonButton").addEventListener('click', () => {
+    document.querySelector('#response').innerHTML = '';
+    getPokemon();
+
+    //changes total pokemon number
+    totalPokemon++;
+    document.querySelector('#totalPokemon').innerHTML = `Total Pokemon: ${totalPokemon + 1}`;
+});
+
+//checks whether the submission is correct
+document.querySelector('#anotherButton').addEventListener('click', () => {
+    if (document.querySelector('#pokemonBox').value.toLowerCase() == pokemonName.toLowerCase()) {
+        document.querySelector('#response').innerHTML = 'Correct';
+        UpScore();
+        hasScored = true;
+    }
+    else {
+        document.querySelector('#response').innerHTML = 'Wrong';
+    }
+});
