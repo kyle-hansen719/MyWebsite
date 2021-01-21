b9d5791d33ed4f21fc3c888f696b0857d06ffcab
Guzzard69 <khan7192020@gmail.com>
2021-01-21 02:24:17 -0500

    added email functionality

--- MyWebsite/app.js
+++ MyWebsite/app.js
@@ -5,9 +5,12 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({
-    extended: false
+    extended: true
 }));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,9 +19,24 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.post('/api', (req, res) => {
-    console.log('bruh');
+//sending emails
+app.post('/', (req, res) => {
+    const msg = {
+        to: 'khan7192020@gmail.com',
+        from: 'khan7192020@gmail.com',
+        subject: `Email From My Website`,
+        text: `Sent by '${req.body.name}' from the email address '${req.body.emailAddress}': ${req.body.message}`,
+        html: `Sent by '${req.body.name}' from the email address '${req.body.emailAddress}': ${req.body.message}`,
+    }
+    sgMail.send(msg).then(() => {
+        console.log('Email sent')
+    }).catch((error) => {
+        console.error(error)
+    })
+
+    res.status(200).redirect('/');
 });
+
 
 const port = process.env.port || 3000;
 app.listen(port, console.log(`listening on port ${port}`));
